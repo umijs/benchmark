78249a4577a61b05b8a61e1ce9977f074ff59b5a
sorrycc <sorrycc@gmail.com>
2024-05-07 11:33:13 +0800

    Add three10x and with-antd projects

---
+++ projects/three10x/src/index-1.ts
@@ -0,0 +1,11 @@
+
+import * as three1 from './copy1/Three.js'; export { three1 }
+import * as three2 from './copy2/Three.js'; export { three2 }
+import * as three3 from './copy3/Three.js'; export { three3 }
+import * as three4 from './copy4/Three.js'; export { three4 }
+import * as three5 from './copy5/Three.js'; export { three5 }
+import * as three6 from './copy6/Three.js'; export { three6 }
+import * as three7 from './copy7/Three.js'; export { three7 }
+import * as three8 from './copy8/Three.js'; export { three8 }
+import * as three9 from './copy9/Three.js'; export { three9 }
+import * as three10 from './copy10/Three.js'; export { three10 }
---
+++ projects/three10x/src/index.ts
@@ -0,0 +1,11 @@
+
+import('./copy1/Three.js');
+import('./copy2/Three.js');
+import('./copy3/Three.js');
+import('./copy4/Three.js');
+import('./copy5/Three.js');
+import('./copy6/Three.js');
+import('./copy7/Three.js');
+import('./copy8/Three.js');
+import('./copy9/Three.js');
+import('./copy10/Three.js');
