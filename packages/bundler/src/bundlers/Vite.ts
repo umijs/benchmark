73d412ccb990a8cd3c535f85ab81d19267ba86e0
xiaohuoni <xiaohuoni@dingtalk.com>
2024-05-08 17:27:39 +0800

    chore: code style

--- packages/bundler/src/bundlers/Vite.ts
+++ packages/bundler/src/bundlers/Vite.ts
@@ -16,19 +16,19 @@ export class Vite extends BaseBundler {
     };
     await vite.build(buildOpts);
   }
-    async dev() {
-      const viteStartTime = performance.now();
-      const buildOpts = {
-        root: this.opts.root,
-        server: { port: 3000 }
-      };
-      const server = await vite.createServer(buildOpts);
-      await server.listen();
-      const info = server.config.logger.info;
-      const startupDurationString = `ready in ${Math.ceil(performance.now() - viteStartTime)} ms`;
-      info(
-        startupDurationString,
-      );
-      server.printUrls();
-    }
+  async dev() {
+    const viteStartTime = performance.now();
+    const buildOpts = {
+      root: this.opts.root,
+      server: { port: 3000 }
+    };
+    const server = await vite.createServer(buildOpts);
+    await server.listen();
+    const info = server.config.logger.info;
+    const startupDurationString = `ready in ${Math.ceil(performance.now() - viteStartTime)} ms`;
+    info(
+      startupDurationString,
+    );
+    server.printUrls();
   }
+}
