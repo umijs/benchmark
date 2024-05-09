6ea32be141b864a35d9b268ddb455dcbe931324b
sorrycc <sorrycc@gmail.com>
2024-05-09 22:49:03 +0800

    feat(bundler): add help and version info for cli

--- packages/bundler/src/cli.ts
+++ packages/bundler/src/cli.ts
@@ -7,6 +7,16 @@ const VALID_COMMANDS = ["build", "dev"];
 
 (async () => {
   let [bundler, command] = argv._ as string[];
+  if (argv.h || argv.help) {
+    showHelp();
+    return;
+  } else if (argv.v || argv.version) {
+    console.log(require("../package.json").version);
+    return;
+  } else if (!bundler || !command) {
+    showHelp();
+    return;
+  }
   assert(VALID_BUILDERS.includes(bundler as string), `Invalid bundler: ${bundler}`);
   assert(VALID_COMMANDS.includes(command as string), `Invalid command: ${command}`);
   let type = bundler[0].toUpperCase() + bundler.slice(1);
@@ -20,3 +30,28 @@ const VALID_COMMANDS = ["build", "dev"];
   console.error(e);
   process.exit(1);
 });
+
+function showHelp() {
+  console.log(`
+  Usage: bundler <bundler> <command> [options]
+
+  Options:
+    -h, --help      Show this message
+    -v, --version   Show version number
+
+  Commands:
+    build           Build the project
+    dev             Start the development server
+
+  Bundlers:
+    mako            Mako
+    vite            Vite
+    webpack         Webpack
+    rsbuild         RSBuild
+    farm            Farm
+
+  Examples:
+    bundler mako build
+    bundler vite dev
+  `);
+}
